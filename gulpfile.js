c96f7f952204e5d3f905be9ab2c33ea5f422aede
Steven Krebs <steven.krebs@ms-visucom.de>
2016-02-17 16:36:09 +0100

    Fixed dependencies in package.json

--- gulpfile.js
+++ gulpfile.js
@@ -11,38 +11,28 @@ var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 
 gulp.task('watch', function() {
-    gulp.watch('dev/js/scripts.js', ['browserify','uglify']);
-    gulp.watch('dev/js/style.less', ['less','minify']);
+    gulp.watch('dev/js/scripts.js', ['browserify']);
+    gulp.watch('dev/js/style.less', ['less']);
 });
 
-gulp.task('build', ['browserify','uglify','less','minify']);
+gulp.task('build', ['browserify','less']);
 
 gulp.task('browserify', function() {
-    return gulp.src('dev/js/scripts.js')
+    return gulp.src('js/scripts.js')
         .pipe(browserify({
             insertGlobals: true
         }))
-        .pipe(rename('scripts.bundle.js'))
-        .pipe(gulp.dest('dev/js/'));
-});
-
-gulp.task('uglify', function(){
-    return gulp.src('dev/js/scripts.bundle.js')
         .pipe(uglify())
         .pipe(rename('scripts.min.js'))
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('js/'));
 });
 
 gulp.task('less', function() {
-    return gulp.src('dev/css/style.less')
+    return gulp.src('css/styles.less')
         .pipe(less())
-        .pipe(rename('styles.css'))
-        .pipe(gulp.dest('dev/css/'));
-});
-
-gulp.task('minify', function() {
-    return gulp.src('dev/css/styles.css')
-        .pipe(minifyCSS())
+        .pipe(minifyCSS({
+            processImport: false
+        }))
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('css/'));
 });
