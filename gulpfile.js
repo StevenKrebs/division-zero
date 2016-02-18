07c9031caea92db45fa77ffb12e6b60aab8e3093
Tim Kopplow <tim@arctium.org>
2016-02-18 10:41:38 +0100

    changed gulpfile and updated readme

--- gulpfile.js
+++ gulpfile.js
@@ -14,40 +14,43 @@ var uglify = require('gulp-uglify');
 var less = require('gulp-less');
 var minifyCSS = require('gulp-minify-css');
 
-gulp.task('watch', function() {
-    gulp.watch('js/scripts.js', ['browserify']);
-    gulp.watch('js/styles.less', ['less']);
-});
-
-gulp.task('sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
-    gulp.watch("index.html").on("change", reload);
-    gulp.watch("css/scripts.min.js").on("change", reload);
-    gulp.watch("js/styles.min.css").on("change", reload);
-});
-
-gulp.task('build', ['browserify','less']);
-
-gulp.task('browserify', function() {
+gulp.task('js', function() {
     return gulp.src('js/scripts.js')
         .pipe(browserify({
             insertGlobals: true
         }))
         .pipe(uglify())
         .pipe(rename('scripts.min.js'))
-        .pipe(gulp.dest('js/'));
+        .pipe(gulp.dest('js/'))
+        .pipe(browserSync.reload({stream:true, once: true}));
 });
 
-gulp.task('less', function() {
+gulp.task('css', function() {
     return gulp.src('css/styles.less')
         .pipe(less())
         .pipe(minifyCSS({
             processImport: false
         }))
         .pipe(rename('styles.min.css'))
-        .pipe(gulp.dest('css/'));
+        .pipe(gulp.dest('css/'))
+        .pipe(browserSync.reload({stream:true}));
+});
+
+gulp.task('browserSync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./",
+        }
+    });
+});
+gulp.task('bs-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('serve', ['css', 'js', 'browserSync'], function () {
+    gulp.watch('js/scripts.js', ['js']);
+    gulp.watch('css/styles.less', ['css']);
+    gulp.watch('index.html', ['bs-reload']);
+});
+gulp.task('build', ['css', 'js'], function () {
 });
