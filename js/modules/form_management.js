24da62bec11e4d7adaa8fdf57517e20c00900a3b
StevenKrebs <krebssteven@gmail.com>
2016-02-21 17:31:33 +0100

    Revamped form management

--- js/modules/form_management.js
+++ js/modules/form_management.js
@@ -5,14 +5,8 @@ $('#submit').click(function() {
     });
     var err = false;
     $('input:not(#submit)').each(function() {
-        var thiz = document.getElementById($(this).attr('id'));
-        if(thiz.checkValidity() == false) {
-            if($(this).attr('type') == 'checkbox') {
-                console.log($(this).find('label'))
-                $(this).next('label').after('<br /><span class="form-input-error">' + $(this).data('error') + '</span>');
-            } else {
-                $(this).after('<span class="form-input-error">' + $(this).data('error') + '</span>');
-            }
+        if(document.getElementById($(this).attr('id')).checkValidity() == false) {
+            $(this).after('<span class="form-input-error">' + $(this).data('error') + '</span>');
             err = true;
             return;
         }
@@ -28,25 +22,22 @@ $('#submit').click(function() {
                 url: $(form).attr('action'),
                 data: formData
             })
-            .done(function(response) {
-                // Set the message text.
-                $(returnMessage).show();
-                $(returnMessage).find('span').text(response);
+        .done(function(response) {
+            // Set the message text.
+            $(returnMessage).show();
+            $(returnMessage).find('span').text(response);
 
-                // Clear the form.
-                $('#application input:not([type="checkbox"])').each(function(){
-                    $(this).val('');
-                });
-                $('#application input[type="checkbox"]').each(function(){
-                    $(this).attr('checked',false);
-                });
-            })
-            .fail(function(data) {
-                $(retryButton).show().css('display', 'block');
-                $(returnMessage).show();
-
-                // Set the message text.
-                $(returnMessage).find('span').text("There was a network outage. Please try again!");
+            // Clear the form.
+            $('#application input:not([type="checkbox"])').each(function(){
+                $(this).val('');
             });
+        })
+        .fail(function(data) {
+            $(retryButton).show().css('display', 'block');
+            $(returnMessage).show();
+
+            // Set the message text.
+            $(returnMessage).find('span').text("There was a network outage. Please try again!");
+        });
     }
 });
