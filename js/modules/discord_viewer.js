30f87289677ba0d760bd1fc2b423b316c19425fc
Steven Krebs <steven.krebs@ms-visucom.de>
2016-02-24 00:55:57 +0100

    Redone discord user-list

--- js/modules/discord_viewer.js
+++ js/modules/discord_viewer.js
@@ -1,33 +1,30 @@
 /* Get Discord Data */
 function setupDiscord() {
     var serverID = $('#discord-serverlink').data('server-id');
-    var div = $('#discord-userlist');
-    var headline = $('#discord-userlist h3');
-    var userlist = $('#discord-userlist ul');
-    $(div).fadeOut('slow');
-    if($(userlist).length > 0) {
-        $(userlist).remove();
+    $('#discord-userlist').fadeOut('slow');
+    if($('#discord-userlist ul').length > 0) {
+        $('#discord-userlist ul').remove();
+    } else {
+        $('#discord-userlist h3').after('<ul></ul>');
     }
-    $(headline).after('<ul></ul>');
-
     $.getJSON("https://discordapp.com/api/servers/" + serverID + "/widget.json", function(data) {
         var membersOnline = data.members.length;
         if (membersOnline > 0) {
             if (membersOnline == 1) {
-                $(headline).text(membersOnline + ' agent online:');
+                $('#discord-userlist h3').text(membersOnline + ' agent online:');
             } else {
-                $(headline).text(membersOnline + ' agents online:');
+                $('#discord-userlist h3').text(membersOnline + ' agents online:');
             }
             $.each(data.members, function() {
                 var game = '';
                 if (this.game) {
                     game = '<span class="user_game">('+ this.game.name +')</span>';
                 }
-                $(userlist).append('<li><img src="' + this.avatar_url + '" class="user_avatar"/><span class="user_status" data-status="'+ this.status +'"></span><span class="user_name" data-role="' + this.discriminator + '">' + this.username + "</span>" + game + "</li>");
+                $('#discord-userlist ul').append('<li><img src="' + this.avatar_url + '" class="user_avatar"/><span class="user_status" data-status="'+ this.status +'"></span><span class="user_name" data-role="' + this.discriminator + '">' + this.username + "</span>" + game + "</li>");
             });
         } else {
-            $(headline).text('No agent online!').addClass('center');
+            $('#discord-userlist h3').text('No agent online!').addClass('center');
         }
-    },$(div).fadeIn('slow'));
+    },$('#discord-userlist').fadeIn('slow'));
 };
 setInterval(setupDiscord(), 60000);
