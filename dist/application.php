3ba7aee45d2393cab213e2c967461628935a368b
StevenKrebs <krebssteven@gmail.com>
2016-03-19 18:22:42 +0100

    Finished form-management

--- dist/application.php
+++ dist/application.php
@@ -1,51 +1,29 @@
 <?php
+    $formData   = file_get_contents( 'php://input' );
+    $data       = json_decode( $formData );
 
-    // Only process POST reqeusts.
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Get the form fields and remove whitespace.
-        $name = strip_tags(trim($_POST["Agent-handle"]));
-				$name = str_replace(array("\r","\n"),array(" "," "),$name);
-        $email = filter_var(trim($_POST["Agent-email"]), FILTER_SANITIZE_EMAIL);
-        $account = trim($_POST["Agent-account"]);
+    $name       = $data->Name;
+    $mail       = $data->EMail;
+    $uplay      = $data->Uplay;
+    $recipient  = $data->recipient;
+    $subject    = $data->subject;
+    $lang       = $data->lang;
 
-        // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($account) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            // Set a 400 (bad request) response code and exit.
-            http_response_code(400);
-            echo "Data incomplete. Program aborted!";
-            exit;
-        }
+    // Build the email content.
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n";
+    $email_content .= "Uplay-Account: $uplay";
+    $email_content .= "Sprache: $lang";
 
-        // Set the recipient email address.
-        // FIXME: Update this to your desired email address.
-        $recipient = "application@division-zero.org";
+    // Build the email headers.
+    $email_headers = "From: $name <$email>";
 
-        // Set the email subject.
-        $subject = "New application for Team ZERO by $name";
-
-        // Build the email content.
-        $email_content = "Name: $name\n";
-        $email_content .= "Email: $email\n";
-        $email_content .= "Uplay-Account: $account";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
-
-        // Send the email.
-        if (mail($recipient, $subject, $email_content, $email_headers)) {
-            // Set a 200 (okay) response code.
-            http_response_code(200);
-            echo "Application received, agent.";
-        } else {
-            // Set a 500 (internal server error) response code.
-            http_response_code(500);
-            echo "Error, please try again!";
-        }
-
+    // Send the email.
+    if (mail($recipient, $subject, $email_content, $email_headers)) {
+        // Set a 200 (okay) response code.
+        http_response_code(200);
     } else {
-        // Not a POST request, set a 403 (forbidden) response code.
-        http_response_code(403);
-        echo "Nice try, rogue.";
+        // Set a 500 (internal server error) response code.
+        http_response_code(500);
     }
-
 ?>
--- src/js/modules/angular/controller/formCtrl.js
+++ src/js/modules/angular/controller/formCtrl.js
@@ -4,12 +4,11 @@ var angular = require('angular'),
 
 // Form management
 app.controller("formCtrl",["$scope", "$http", "$translate", function($scope, $http, $translate) {
-    $scope.applicationResult    = null,
-    $scope.success              = null,
-    $scope.error                = null,
-    $scope.submitApplication    = function(form) {
+    $scope.success              = false,
+    $scope.error                = false,
+    $scope.submitApplication    = function() {
         $scope.submitted        = true;
-        if(form.$valid) {
+        if($scope.application.$valid) {
             var data = {
                 "recipient" :   "application@division-zero.org",
                 "subject"   :   "Neue Bewerbung für Team /0/ von " + $scope.formInputNickname,
@@ -18,78 +17,21 @@ app.controller("formCtrl",["$scope", "$http", "$translate", function($scope, $ht
                 "Uplay"     :   $scope.formInputUplay,
                 "lang"      :   $translate.proposedLanguage()
             };
-
             $http({
                 method      :   'POST',
-                url         :   'application.php',
+                url         :   '/application.php',
                 data        :   data
             })
                 .then(function(response) {
                     $scope.success              = true;
-                    $scope.applicationResult    = response;
                 },
                 function(response) {
                     $scope.error                = true;
-                    $scope.applicationResult    = response;
                 });
         }
+    };
+    $scope.retry                = function() {
+        $scope.success          = false;
+        $scope.error            = false;
     }
 }]);
-
-
-// $('#submit').click(function() {
-//     $('.form-input-error').each(function() {
-//         $(this).remove();
-//     });
-//     var err = false;
-//     $('input:not(#submit)').each(function() {
-//         if(document.getElementById($(this).attr('id')).checkValidity() == false) {
-//             $(this).after('<span class="form-input-error">' + $(this).data('error') + '</span>');
-//             err = true;
-//             return;
-//         }
-//     });
-//
-//     var form = $('#application');
-//     var returnMessage = $('#form-result');
-//     var retryButton = $('#form-result #retry');
-//     var formData = $(form).serialize();
-//
-//     if (err == false && localStorage.applied != "true") {
-//         $('#form').hide();
-//         $.ajax({
-//                 type: 'POST',
-//                 url: $(form).attr('action'),
-//                 data: formData
-//             })
-//         .done(function(response) {
-//             // Set the message text.
-//             $(returnMessage).show();
-//             $(returnMessage).find('#form-result-text').text(response);
-//
-//             // Clear the form.
-//             $('#application input').each(function(){
-//                 $(this).val('');
-//             });
-//
-//             //Attempt to limit spam
-//             localStorage.applied = true;
-//         })
-//         .fail(function(data) {
-//             $(retryButton).show().css('display', 'block');
-//             $(returnMessage).show();
-//
-//             // Set the message text.
-//             $(returnMessage).find('#form-result-text').text("Error, please try again!");
-//         });
-//         return false
-//     } else if(localStorage.applied == "true") {
-//         $('#form').hide();
-//
-//         $(retryButton).show().css('display', 'block');
-//         $(returnMessage).show();
-//
-//         // Set the message text.
-//         $(returnMessage).find('#form-result-text').text("Application already received!");
-//     }
-// });
