91bb849d1cff566735130e4c43bd8aabc65d3ebf
StevenKrebs <krebssteven@gmail.com>
2016-03-23 22:30:46 +0100

    Small fix for scrollPos-directive

--- src/js/modules/_controller/navCtrl.js
+++ src/js/modules/_controller/navCtrl.js
@@ -9,11 +9,11 @@ app.controller('navCtrl',['$scope','$window','scrollPos', function($scope, $wind
         $scope.mobile = true;
     }
     angular.element($window).bind("scroll", function() {
-        $scope.scrollPos = $(document).scrollTop() + $(window).height() * .9;
+        $scope.scrollPos = $(document).scrollTop() + $(window).height()/2;
         if($scope.desktop == true) {
-            scrollPos.getScrollPos($scope.scrollPos, $('.desktop-nav span'));
+            scrollPos.getScrollPos($scope.scrollPos, $('.desktop-nav span'), 'scroll-to-scene');
         } else {
-            scrollPos.getScrollPos($scope.scrollPos, $('.mobile-nav span'));
+            scrollPos.getScrollPos($scope.scrollPos, $('.mobile-nav span'), 'scroll-to-scene');
         }
     });
 }]);
--- src/js/modules/_services/getScrollPos.js
+++ src/js/modules/_services/getScrollPos.js
@@ -3,14 +3,17 @@ var angular         = require('angular'),
     config          = require('../config.js');
 
 app.factory('scrollPos', function() {
-   function getScrollPos(scrollPos, menuItems) {
+   function getScrollPos(scrollPos, menuItems, identifier) {
        $(menuItems).each(function() {
            var link = $(this),
-               href = $(link.attr('scroll-to-scene')),
+               href = $(link.attr(identifier)),
                topPos = $(href).position().top,
                bottomPos = $(href).position().top + $(href).outerHeight();
            if (scrollPos > topPos && scrollPos <= bottomPos) {
                link.addClass('highlight');
+           } else if ($(document).scrollTop() + $(window).height() * 0.9 > $($(menuItems).last().attr(identifier)).position().top) {
+               $(menuItems).removeClass('highlight');
+               $(menuItems).last().addClass('highlight');
            } else {
                link.removeClass('highlight');
            }
